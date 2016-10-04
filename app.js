37930aa11859433003402ec7e30e3b54db0adc3d
ArguelBenoit <benoit.arguel@gmail.com>
2016-10-04 18:59:02 +0200

    init project

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+    socketio = require('socket.io'),
+    app = express(),
+    server = app.listen(8080),
+    io = socketio.listen(server);
+
+app.use(express.static('public'));
+
+io.on('connection', function (socket) {
+  socket.on('message', function (message) {
+    socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+  });
+});
