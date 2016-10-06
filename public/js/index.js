3bff2d00f01ec7abb96d359c66fd216710fe96d9
BenoitArguel <benoit.arguel@gmail.com>
2016-10-06 15:34:43 +0200

    task: adding bottom scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,11 @@
 
-
 var socket = io.connect('http://localhost:8080');
 var pseudo = prompt('Quel est votre pseudo ?');
-// var pseudo = 'admin';
 socket.on('message', function(data) {
   insereMessage(data.pseudo, (data.message).split("\n").join('<br/>'))
 })
+
+
 $('#formulaire_chat').submit(function () {
   var message = $('#message').val();
   message = (message).split("\n").join('<br/>');
@@ -15,11 +15,13 @@ $('#formulaire_chat').submit(function () {
   $('#message').val('').focus();
   return false;
 });
+
+
 function insereMessage(thisPseudo, message) {
   var forMeOrNot = message.indexOf('@'+ pseudo);
   var important = message.indexOf('@important');
   if (forMeOrNot > 0) {
-    $('#zone_chat').append('<p class="message-for-me"><strong class="important">' + thisPseudo + ' <i class="fa fa-comment-o" aria-hidden="true"></i></strong></br>Pour vous : ' + message + '</p>');
+    $('#zone_chat').append('<p class="message-for-me"><strong class="important">' + thisPseudo + ' (Pour vous) <i class="fa fa-comment-o" aria-hidden="true"></i></strong></br>' + message + '</p>');
   } else if(important > 0) {
     $('#zone_chat').append('<p class="important"><strong class="important">' + thisPseudo + ' <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></strong></br>' + message + '</p>');
   } else if (thisPseudo == pseudo) {
@@ -27,4 +29,6 @@ function insereMessage(thisPseudo, message) {
   } else {
     $('#zone_chat').append('<p class="p-other-users"><strong class="other">' + thisPseudo + ' ' + '<i class="fa fa-comment-o" aria-hidden="true"></i></strong></br>' + message + '</p>');
   }
+  var element = document.getElementById('body');
+  element.scrollTop = element.scrollHeight;
 }
