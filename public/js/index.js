ae2af084e892d84d77db780f1f298ca7e2f5e944
BenoitArguel <benoit.arguel@gmail.com>
2016-10-07 15:54:03 +0200

    feat: add disconnect button

--- public/js/index.js
+++ public/js/index.js
@@ -2,16 +2,20 @@ var socket = io.connect('http://localhost:8080');
 var pseudo = prompt('Quel est votre pseudo ?');
 document.title = pseudo;
 
+
+$('#disconnect').html( '<span>' + pseudo + ' ' +'</span><img src="img/cross.png" height="14px" width="14px"/>' );
+$('#disconnect').on('click', function() { location.reload(); });
+
+
 function insereFormatMessage(thisPseudo, message, dateMessage) {
   var forMeOrNot = message.indexOf('@'+ pseudo);
   var important = message.indexOf('@important');
   message = (message).split("&#10;").join('<br/>');
-  message = (message).replace(/(@\S+)/, '<span class="span-target">$1</span>');
   message = (message).replace(/(http:\S+)/, '<a href="$1">$1</a>');
   message = (message).replace(/(https:\S+)/, '<a href="$1">$1</a>');
-  if (forMeOrNot > 0) {
+  if (forMeOrNot >= 0) {
     $('#zone_chat').append('<p class="message-for-me"><strong class="important">' + thisPseudo + ' (Pour vous) <i class="fa fa-comment-o" aria-hidden="true"></i></strong></br>' + message + '<span class="date"> ' + dateMessage + '</span></p>');
-  } else if(important > 0) {
+  } else if(important >= 0) {
     $('#zone_chat').append('<p class="important"><strong class="important">' + thisPseudo + ' <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></strong></br>' + message + '<span class="date"> ' + dateMessage + '</span></p>');
   } else if (thisPseudo == pseudo) {
     $('#zone_chat').append('<p class="p-my-user"><strong class="my">' + thisPseudo + ' (Moi-même) <i class="fa fa-comment-o" aria-hidden="true"></i></strong></br>' + message + '<span class="date"> ' + dateMessage + '</span></p>');
@@ -22,17 +26,20 @@ function insereFormatMessage(thisPseudo, message, dateMessage) {
   body.scrollTop = body.scrollHeight;
 }
 
+
 socket.on('sendAllMessages', function(data) {
   insereFormatMessage(data.pseudo, data.message, data.date);
 });
-
 socket.on('message', function(data) {
   insereFormatMessage(data.pseudo, data.message, data.date);
 });
-
 $('#formulaire_chat').submit(function () {
   var message = $('#message').val();
-  socket.emit('message', {message: message, pseudo: pseudo});
+  if(message) {
+    socket.emit('message', {message: message, pseudo: pseudo});
+    $('#message').val('').focus();
+    return false;
+  }
   $('#message').val('').focus();
   return false;
 });
