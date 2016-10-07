afbe95e66c7d4d3e877053d98b398042b3f1145e
BenoitArguel <benoit.arguel@gmail.com>
2016-10-07 10:00:45 +0200

    task: use ent for be carefull

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var express = require('express'),
     app = express(),
     redis = require('redis'),
     server = app.listen(8080),
+    ent = require('ent'),
     io = socketio.listen(server),
     client = redis.createClient();
 
 app.use(express.static('public'));
 
 io.on('connection', function (socket) {
+  ///// old messages
   client.lrange('dubois', 0, -1, (err, result) => {
     result.forEach((element) => {
       var elementSplited = element.split('~$@~'),
@@ -17,9 +19,12 @@ io.on('connection', function (socket) {
       socket.emit('sendAllMessages', {pseudo: pseudo, message: message});
     });
   });
+  ///// new messages
   socket.on('message', (data) => {
-    client.lpush('dubois', data.pseudo + ' ~$@~ ' + data.message);
-    client.ltrim('dubois', 0, 399);
+    data.message = ent.encode(data.message);
+    socket.emit('message', {pseudo: data.pseudo, message: data.message});
     socket.broadcast.emit('message', {pseudo: data.pseudo, message: data.message});
+    client.rpush('dubois', data.pseudo + '~$@~' + data.message);
+    client.ltrim('dubois', 0, 399);
   });
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,14 @@
 var socket = io.connect('http://localhost:8080');
 var pseudo = prompt('Quel est votre pseudo ?');
 
-socket.on('sendAllMessages', function(data) {
-  insereMessage(data.pseudo, (data.message).split("\n").join('<br/>'));
-});
-
-socket.on('message', function(data) {
-  insereMessage(data.pseudo, (data.message).split("\n").join('<br/>'));
-});
-
-
-$('#formulaire_chat').submit(function () {
-  var message = $('#message').val();
-  message = (message).split("\n").join('<br/>');
-  message = (message).replace(/(@\S+)/, '<span class="span-target">$1</span>');
-  socket.emit('message', {message: message, pseudo: pseudo});
-  insereMessage(pseudo, message);
-  $('#message').val('').focus();
-  return false;
-});
-
-
-
-function insereMessage(thisPseudo, message) {
+function insereFormatMessage(thisPseudo, message) {
   var dateMessage = moment().format(' DD/MM  HH:mm');
-  console.log(dateMessage);
   var forMeOrNot = message.indexOf('@'+ pseudo);
   var important = message.indexOf('@important');
+  message = (message).split("&#10;").join('<br/>');
+  message = (message).replace(/(@\S+)/, '<span class="span-target">$1</span>');
+  message = (message).replace(/(http:\S+)/, '<a href="$1">$1</a>');
+  message = (message).replace(/(https:\S+)/, '<a href="$1">$1</a>');
   if (forMeOrNot > 0) {
     $('#zone_chat').append('<p class="message-for-me"><strong class="important">' + thisPseudo + ' (Pour vous) <i class="fa fa-comment-o" aria-hidden="true"></i></strong></br>' + message + '<span class="date"> ' + dateMessage + '</span></p>');
   } else if(important > 0) {
@@ -39,3 +21,18 @@ function insereMessage(thisPseudo, message) {
   var body = document.getElementById('body');
   body.scrollTop = body.scrollHeight;
 }
+
+socket.on('sendAllMessages', function(data) {
+  insereFormatMessage(data.pseudo, data.message);
+});
+
+socket.on('message', function(data) {
+  insereFormatMessage(data.pseudo, data.message);
+});
+
+$('#formulaire_chat').submit(function () {
+  var message = $('#message').val();
+  socket.emit('message', {message: message, pseudo: pseudo});
+  $('#message').val('').focus();
+  return false;
+});
