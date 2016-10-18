1ea169e84db3dcd0ddba839cdd8d9658194fd71a
BenoitArguel <benoit.arguel@gmail.com>
2016-10-18 15:09:13 +0200

    fix: old posts dosen't work in firefox

--- views/js/chat.js
+++ views/js/chat.js
@@ -1,45 +1,47 @@
-var socket = io.connect('http://'+ window.location.hostname +':3000/');
-
-function insereFormatMessage(thisPseudo, message, dateMessage, pseudo) {
-  var forMeOrNot = message.indexOf('@'+ pseudo);
-  var important = message.indexOf('@important');
-  message = (message).split("&#10;").join('<br/>');
-  message = (message).replace(/(http:\S+)/, '<a href="$1">$1</a>');
-  message = (message).replace(/(https:\S+)/, '<a href="$1">$1</a>');
-  if (forMeOrNot >= 0) {
-    $('#zone_chat').append('<p class="message-for-me"><strong class="important">' + thisPseudo + ' (Pour vous) <i class="fa fa-comment-o" aria-hidden="true"></i></strong></br>' + message + '<span class="date"> ' + dateMessage + '</span></p>');
-  } else if(important >= 0) {
-    $('#zone_chat').append('<p class="important"><strong class="important">' + thisPseudo + ' <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></strong></br>' + message + '<span class="date"> ' + dateMessage + '</span></p>');
-  } else if (thisPseudo == pseudo) {
-    $('#zone_chat').append('<p class="p-my-user"><strong class="my">' + thisPseudo + ' (Moi-même) <i class="fa fa-comment-o" aria-hidden="true"></i></strong></br>' + message + '<span class="date"> ' + dateMessage + '</span></p>');
-  } else {
-    $('#zone_chat').append('<p class="p-other-users"><strong class="other">' + thisPseudo + ' ' + '<i class="fa fa-comment-o" aria-hidden="true"></i></strong></br>' + message + '<span class="date"> ' + dateMessage + '</span></p>');
-  }
-  document.getElementById('body').scrollTop = document.getElementById('body').scrollHeight;
-}
-
-/*___________________________________________________*/
 
 $(document).ready(function(){
+
+  var socket = io.connect('http://'+ window.location.hostname +':3000/');
   var pseudoCourant;
+
+  function insereFormatMessage(thisPseudo, message, dateMessage, pseudo) {
+    var forMeOrNot = message.indexOf('@'+ pseudo);
+    var important = message.indexOf('@important');
+    message = (message).split("&#10;").join('<br/>');
+    message = (message).replace(/(http:\S+)/, '<a href="$1">$1</a>');
+    message = (message).replace(/(https:\S+)/, '<a href="$1">$1</a>');
+    if (forMeOrNot >= 0) {
+      $('#zone_chat').append('<p class="message-for-me"><strong class="important">' + thisPseudo + ' (Pour vous) <i class="fa fa-comment-o" aria-hidden="true"></i></strong></br>' + message + '<span class="date"> ' + dateMessage + '</span></p>');
+    } else if(important >= 0) {
+      $('#zone_chat').append('<p class="important"><strong class="important">' + thisPseudo + ' <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></strong></br>' + message + '<span class="date"> ' + dateMessage + '</span></p>');
+    } else if (thisPseudo == pseudo) {
+      $('#zone_chat').append('<p class="p-my-user"><strong class="my">' + thisPseudo + ' (Moi-même) <i class="fa fa-comment-o" aria-hidden="true"></i></strong></br>' + message + '<span class="date"> ' + dateMessage + '</span></p>');
+    } else {
+      $('#zone_chat').append('<p class="p-other-users"><strong class="other">' + thisPseudo + ' ' + '<i class="fa fa-comment-o" aria-hidden="true"></i></strong></br>' + message + '<span class="date"> ' + dateMessage + '</span></p>');
+    }
+    document.getElementById('body').scrollTop = document.getElementById('body').scrollHeight;
+  }
+
   $.post("http://localhost:3000/sess", false, function(a) {
     $('#disconnect').html( '<span>' + a + ' ' +'</span><img src="img/cross.png" height="14px" width="14px"/>' );
     $('#disconnect').on('click', function(){window.location.replace('http://'+ window.location.hostname +':3000/logout');});
     pseudoCourant = a;
-  });
-  $.post("http://localhost:3000/mess", false, function(a) {
-    var allMessages = JSON.parse(a);
-    allMessages.forEach((element) => {
-      var elementSplited = element.split('~$@~'),
-          pseudo = elementSplited[0],
-          message = elementSplited[1],
-          date = elementSplited[2];
-      insereFormatMessage(pseudo, message, date, pseudoCourant);
+    $.post("http://localhost:3000/mess", false, function(a) {
+      var allMessages = JSON.parse(a);
+      allMessages.forEach((element) => {
+        var elementSplited = element.split('~$@~'),
+        pseudo = elementSplited[0],
+        message = elementSplited[1],
+        date = elementSplited[2];
+        insereFormatMessage(pseudo, message, date, pseudoCourant);
+      });
     });
   });
+
   socket.on('message', function(data) { /// received one message
     insereFormatMessage(data.pseudo, data.message, data.date, pseudoCourant);
   });
+
   $('#formulaire_chat').submit(function () { /// send one message
     var message = $('#message').val();
     if(message && pseudoCourant != undefined) {
@@ -50,4 +52,5 @@ $(document).ready(function(){
     $('#message').val('').focus();
     return false;
   });
+
 });
