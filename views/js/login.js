a99877f2a82b5a97335500e3b9a1766a8e84944b
BenoitArguel <benoit.arguel@gmail.com>
2016-10-19 16:35:03 +0200

    task: add link socket.io for nginx

--- views/js/login.js
+++ views/js/login.js
@@ -15,7 +15,11 @@ $(document).ready(function(){
 		code=$('#code').val();
 		$.post('http://'+ window.location.hostname +':3000/chat', { user: user, code: code}, function(data) {
 			if(data === 'done') {
-				window.location.href = 'http://'+ window.location.hostname +'/chat.html';
+				if (window.location.hostname == 'localhost') {
+					window.location.href = 'http://localhost:3000/chat.html';
+				} else {
+					window.location.href = 'http://'+ window.location.hostname +'/chat.html';
+				}
 			} else if (data === 'false') {
 				$('#code').css('border-color', 'rgba(255,0,0,0.6)');
 			}
