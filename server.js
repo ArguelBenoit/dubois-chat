3a7fe7b6bea97483bcd4e652f8cbc2683f0b0c57
BenoitArguel <benoit.arguel@gmail.com>
2016-10-19 16:00:27 +0200

    fix: dynamic url

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var client = redis.createClient();
 
 app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
 	next();
 });
